bc6dd40623a0f965a4930bb39c2c141e09e1135f
udaybhan <guptaudaybhan02@gmail.com>
2021-02-14 21:42:49 +0530

    two-way-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AbcComponent } from './abc/abc.component';
 import { PassDataInEventComponent } from './pass-data-in-event/pass-data-in-event.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     AbcComponent,
-    PassDataInEventComponent
+    PassDataInEventComponent,
+    TwoWayBindingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
