91b71c6dc3213609bb23ef30fd8efe09bf2f26e6
udaybhan <guptaudaybhan02@gmail.com>
2021-02-15 09:04:25 +0530

    login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   model: any = {};
-
+  loginForm: FormGroup | undefined;
   logIn() {
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model));
   }
