c6c554add8e0bba13a3a7c24a74815eb1329bee1
udaybhan <guptaudaybhan02@gmail.com>
2021-02-18 21:25:32 +0530

    fjf

--- src/app/verification/verification.component.ts
+++ src/app/verification/verification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-verification',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./verification.component.css']
 })
 export class VerificationComponent implements OnInit {
-
-  constructor() { }
+  email:any = "";
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const params$ = this.activatedRoute.queryParams;
+    params$.subscribe(data=> {
+      console.log(data);
+      this.email  = data.email;
+    })
   }
 
 }
